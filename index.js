31fad60d99350bbb95cf7d1135f32a59ad27d8bc
zxy <903951682@qq.com>
2022-11-19 15:45:37 +0800

    创建了文件

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: zxy 903951682@qq.com
+ * @Date: 2022-11-19 15:44:14
+ * @LastEditors: zxy 903951682@qq.com
+ * @LastEditTime: 2022-11-19 15:44:46
+ * @FilePath: \test\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('测试')
